39a2a652b7ff91d810415ee140070b1701501d7a
ahmed1997164 <ahmed1997164@gmail.com>
2018-11-26 00:11:48 +0200

    Till 1024Px Done
    Took 4 hours 37 minutes

--- js/plugins.js
+++ js/plugins.js
@@ -18,7 +18,7 @@ $(".owl-products")
 				items: 1
 			},
 			1024: {
-				items: 2,
+				items: 3,
 				center: false,
 				margin: 10
 			},
@@ -34,3 +34,24 @@ $(".owl-products .owl-next")
 	.empty()
 	.append('<div class="row h-100"><div class="col my-auto"><i class="fas fa-chevron-right owlsizeicon"></i></div></div>');
 $(".owl-dots").wrap("<div class='owl-dots-con'></div>");
+
+$(function () {
+	$('.navbar-1').data('size', 'big');
+});
+
+
+window.onscroll = function () {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+		document.getElementById("navBarOne").style.height = "90px";
+		document.getElementById("navBarTwo").style.height = "40px";
+		document.getElementById("navBarTwo").style.top = "90px";
+	} else {
+		document.getElementById("navBarOne").style.height = "140px";
+		document.getElementById("navBarTwo").style.height = "70px";
+		document.getElementById("navBarTwo").style.top = "140px";
+	}
+}
