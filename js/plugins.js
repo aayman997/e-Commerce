3412679461ddb5f08d289255fb45b13ff81c0801
ahmed1997164 <ahmed1997164@gmail.com>
2018-12-02 02:57:41 +0200

    SideMenu
    Took 5 hours 40 minutes

--- js/plugins.js
+++ js/plugins.js
@@ -1,5 +1,21 @@
+const homeHeader = $(".homePage .header"),
+	vph = window.innerHeight;
+console.log(vph);
 if ($("body").hasClass("homePage")) {
-	document.querySelector(".homePage .header").style.height = "calc(100vh - (210px))";
+	if (window.outerWidth > 768) {
+		const totalNavsHeight = $("#navBarOne").outerHeight() + $("#navBarTwo").outerHeight();
+		homeHeader.css({
+			"height": vph - totalNavsHeight,
+			"top": totalNavsHeight
+		});
+		console.log(homeHeader.height());
+	} else if (window.outerWidth <= 768) {
+		const lgHeight = $(".lg-navbar").outerHeight();
+		homeHeader.css({
+			"height": vph - lgHeight,
+			"top": lgHeight
+		});
+	}
 }
 $(".owl-products")
 	.owlCarousel({
@@ -55,3 +71,31 @@ function scrollFunction() {
 		document.getElementById("navBarTwo").style.top = "140px";
 	}
 }
+
+//Side Menu Settings
+if (window.innerWidth <= 768) {
+
+}
+$(".lg-navbar .fa-bars").on("click", function () {
+	$(".side-menu").animate({width: '100vw', "min-height": "100vh"}, 400);
+	$(".side-menu-con").delay(200).fadeTo(400, 1);
+});
+$(".side-menu .close-menu p").on("click", function () {
+	$(".side-menu-con").fadeTo(400, 0);
+	$(this).parents(".side-menu").delay(100).animate({width: '0', "min-height": "0"}, 400);
+});
+$(".sec-header").on("click", function () {
+	if ($(this).hasClass("opened")) {
+		$(this).removeClass("opened");
+		$(this).addClass("closed");
+		$(this).children("i").removeClass("fa-minus");
+		$(this).children("i").addClass("fa-plus");
+		$(this).siblings(".inner-sections").slideUp(200);
+	} else if ($(this).hasClass("closed")) {
+		$(this).removeClass("closed");
+		$(this).addClass("opened");
+		$(this).children("i").removeClass("fa-plus");
+		$(this).children("i").addClass("fa-minus");
+		$(this).siblings(".inner-sections").slideDown(200);
+	}
+});
